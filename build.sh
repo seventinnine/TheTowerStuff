8111f7a69f2969edef23f7ea4cf99a4b06c0ef45
seventinnine <weiensteiner.stefan@gmail.com>
2025-08-03 16:43:49 +0200

    build script fixes

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+cd ./Website
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
-cd Website
 ./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
 ./dotnet/dotnet publish -c Release -o output
